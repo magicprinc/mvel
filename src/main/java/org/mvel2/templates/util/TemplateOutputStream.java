78f6ab200f41d855b4099ed994f7fbdbd93850b6
A.Fink <aprpda@gmail.com>
2025-09-27 10:10:01 +0300

    TemplateOutputStream: more ideas

--- src/main/java/org/mvel2/templates/util/TemplateOutputStream.java
+++ src/main/java/org/mvel2/templates/util/TemplateOutputStream.java
@@ -1,10 +1,18 @@
 package org.mvel2.templates.util;
 
-///  todo {@link Appendable} + Unicode tests
+///  todo {@link Appendable} + Unicode tests + Qute examples +? Vert.x MVEL Template examples? + Click access examples, or accessor
 public interface TemplateOutputStream extends Appendable {
 
 	@Override
 	TemplateOutputStream append (CharSequence c);
 
   TemplateOutputStream append (char[] c);
+
+	@Override
+	Appendable append (CharSequence csq, int start, int end);
+
+	@Deprecated  @Override
+	default TemplateOutputStream append (char c) {
+		return append(new char[]{c});
+	}
 }
